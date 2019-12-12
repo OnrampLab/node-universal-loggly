42ca97226faacd9bc0a30474da06032cc30add4e
glenn <glenn.profile@gmail.com>
2019-12-12 14:50:51 +0800

    feat(library): [EnvInspect] environment Inspect added

---
+++ src/lib/env-inspect/EnvInspect.ts
@@ -0,0 +1,21 @@
+export class EnvInspect {
+  /**
+   * is web environment
+   *
+   * @returns {boolean}
+   */
+  static isWeb() {
+    if (typeof window === 'undefined') {
+      return false;
+    }
+    if (typeof document === 'undefined') {
+      return false;
+    }
+
+    return true;
+  }
+
+  // ============================================================
+  //  private
+  // ============================================================
+}
