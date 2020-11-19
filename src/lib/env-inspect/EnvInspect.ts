8f84c9c3259ddc2c947c5871ef91cb5f85872322
Paul Lee <e0991100238@gmail.com>
2020-11-19 12:13:30 +0800

    refactor: isWeb detection

--- src/lib/env-inspect/EnvInspect.ts
+++ src/lib/env-inspect/EnvInspect.ts
@@ -1,18 +1,9 @@
 export class EnvInspect {
   /**
    * is web environment
-   *
-   * @returns {boolean}
    */
   static isWeb() {
-    if (typeof window === 'undefined') {
-      return false;
-    }
-    if (typeof document === 'undefined') {
-      return false;
-    }
-
-    return true;
+    return typeof window !== 'undefined'
   }
 
   // ============================================================
